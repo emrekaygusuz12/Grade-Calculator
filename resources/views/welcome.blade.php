bf86d33a96527cf92b699b45a26c6e55d3166446
emrekaygusuz <kaygusuz.emreuk@gmail.com>
2024-06-11 22:01:13 +0100

    initialise

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <body>
+        <h1>Welcome to grade calculator!</h1>
+    </body>
+   
+    <p>Number of modules</p>
+            <select name="Modules"> 
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+            </select><br>
+
+            <p>Ucas Points<label for="grade"></label></p>
+
+            
+
+</html>
